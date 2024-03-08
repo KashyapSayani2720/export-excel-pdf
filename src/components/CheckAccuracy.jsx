229dbf9025e2a7d0fa2310e21ab4053be202eb80
Kashyap Sayani <kashyapsayani1@gmail.com>
2024-03-08 19:29:52 +0530

    Added List if names display and added closest address

--- src/components/CheckAccuracy.jsx
+++ src/components/CheckAccuracy.jsx
@@ -15,11 +15,13 @@ const data = {
 
 const CheckAccuracy = () => {
   const [accuracy, setAccuracy] = useState("");
+  const [names, setNames] = useState([]);
+  const [closestAddress, setClosestAddress] = useState({});
   const toast = useRef(null);
   const validationSchema = yup.object().shape({
     pincode: yup.string().required("Pincode is required"),
     city: yup.string().required("City is required"),
-    addressLineOne: yup.string().required("Addres One is required"),
+    addressLineOne: yup.string().required("Address One is required"),
     addressLineTwo: yup.string().required("Address Two is required"),
   });
 
@@ -35,6 +37,8 @@ const CheckAccuracy = () => {
       .then((response) => {
         if (response?.data) {
           setAccuracy(response?.data?.accuracy);
+          setNames(response?.data?.names || []);
+          setClosestAddress(response?.data?.address || {});
           toast.current.show({
             severity: "success",
             summary: "Success",
@@ -48,6 +52,8 @@ const CheckAccuracy = () => {
         console.log("error", error);
         let err = error?.response?.data?.error;
         setAccuracy(null);
+        setNames([]);
+        setClosestAddress({});
         toast.current.show({
           severity: "error",
           summary: "Error",
@@ -75,114 +81,161 @@ const CheckAccuracy = () => {
   };
 
   return (
-    <div className="accuracy-component">
-      <h4>Find Address Accuracy</h4>
-      <Toast ref={toast} />
-      <div>
-        <InputComponent
-          value={values?.pincode}
-          onChange={handleChange}
-          type="text"
-          placeholder="Enter Pincode"
-          name="pincode"
-          error={errors?.pincode}
-          touched={touched?.pincode}
-          className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
-        />
-      </div>
-      <div>
-        <InputComponent
-          value={values?.city}
-          onChange={handleChange}
-          type="text"
-          placeholder="Enter City"
-          name="city"
-          error={errors?.city}
-          touched={touched?.city}
-          className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
-        />
-      </div>
-      <div>
-        <InputComponent
-          value={values?.addressLineOne}
-          onChange={handleChange}
-          type="text"
-          placeholder="Enter Address One"
-          name="addressLineOne"
-          error={errors?.addressLineOne}
-          touched={touched?.addressLineOne}
-          className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
-        />
-      </div>
-      <div>
-        <InputComponent
-          value={values?.addressLineTwo}
-          onChange={handleChange}
-          type="addressLineTwo"
-          placeholder="Enter Address Two"
-          name="addressLineTwo"
-          error={errors?.addressLineTwo}
-          touched={touched?.addressLineTwo}
-          className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
-        />
-      </div>
-      <ButtonComponent
-        onClick={() => handleSubmit()}
-        type="submit"
-        label="Submit"
-        className="px-6 py-2 w-full rounded bg-[#1f1f70] text-white"
-      />
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div className="accuracy-component">
+            <h4>Find Address Accuracy</h4>
+            <Toast ref={toast} />
+            <div>
+              <InputComponent
+                value={values?.pincode}
+                onChange={handleChange}
+                type="text"
+                placeholder="Enter Pincode"
+                name="pincode"
+                error={errors?.pincode}
+                touched={touched?.pincode}
+                className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
+              />
+            </div>
+            <div>
+              <InputComponent
+                value={values?.city}
+                onChange={handleChange}
+                type="text"
+                placeholder="Enter City"
+                name="city"
+                error={errors?.city}
+                touched={touched?.city}
+                className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
+              />
+            </div>
+            <div>
+              <InputComponent
+                value={values?.addressLineOne}
+                onChange={handleChange}
+                type="text"
+                placeholder="Enter Address One"
+                name="addressLineOne"
+                error={errors?.addressLineOne}
+                touched={touched?.addressLineOne}
+                className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
+              />
+            </div>
+            <div>
+              <InputComponent
+                value={values?.addressLineTwo}
+                onChange={handleChange}
+                type="addressLineTwo"
+                placeholder="Enter Address Two"
+                name="addressLineTwo"
+                error={errors?.addressLineTwo}
+                touched={touched?.addressLineTwo}
+                className="text-[12px] rounded  px-[1rem] py-[8px] focus:outline-none w-full border-[1px] border-[#ddd]"
+              />
+            </div>
+            <ButtonComponent
+              onClick={() => handleSubmit()}
+              type="submit"
+              label="Submit"
+              className="px-6 py-2 w-full rounded bg-[#1f1f70] text-white"
+            />
 
-      <div className="main">
-        <div className="label">
-          <div>Low Risk(100)</div>
-          <div>High Risk(0)</div>
+            <div className="main">
+              <div className="label">
+                <div>Low Risk(100)</div>
+                <div>High Risk(0)</div>
+              </div>
+
+              <div className="container">
+                <div
+                  className={`range ${rangeHandler(80, 100) ? "active" : ""}`}
+                >
+                  <div className="text">{accuracy}</div>
+                  <div
+                    className="box"
+                    style={{
+                      background: "linear-gradient(to right, #33FFB2, green)",
+                    }}
+                  ></div>
+                </div>
+                <div
+                  className={`range ${rangeHandler(60, 80) ? "active" : ""}`}
+                >
+                  <div className="text">{accuracy}</div>
+                  <div
+                    className="box"
+                    style={{
+                      background: "linear-gradient(to right, #33FF57, #33FFB2)",
+                    }}
+                  ></div>
+                </div>
+                <div
+                  className={`range ${rangeHandler(40, 60) ? "active" : ""}`}
+                >
+                  <div className="text">{accuracy}</div>
+                  <div
+                    className="box"
+                    style={{
+                      background: "linear-gradient(to right, #FFE833, #00FF00)",
+                    }}
+                  ></div>
+                </div>
+                <div
+                  className={`range ${rangeHandler(20, 40) ? "active" : ""}`}
+                >
+                  <div className="text">{accuracy}</div>
+                  <div
+                    className="box"
+                    style={{
+                      background: "linear-gradient(to right, #FF8333, #FFB233)",
+                    }}
+                  ></div>
+                </div>
+                <div className={`range ${rangeHandler(0, 20) ? "active" : ""}`}>
+                  <div className="text">{accuracy}</div>
+                  <div
+                    className="box"
+                    style={{
+                      background: "linear-gradient(to right, red, #FF5733)",
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="col-6">
+          {/* Display names and closest address */}
+          {names.length > 0 && (
+            <div>
+              <h4>Names:</h4>
+              <ul>
+                {names.map((name, index) => (
+                  <li key={index}>{name}</li>
+                ))}
+              </ul>
+            </div>
+          )}
 
-        <div className="container">
-          <div className={`range ${rangeHandler(80, 100) ? "active" : ""}`}>
-            <div className="text">{accuracy}</div>
-            <div
-              className="box"
-              style={{
-                background: "linear-gradient(to right, #33FFB2, green)",
-              }}
-            ></div>
-          </div>
-          <div className={`range ${rangeHandler(60, 80) ? "active" : ""}`}>
-            <div className="text">{accuracy}</div>
-            <div
-              className="box"
-              style={{
-                background: "linear-gradient(to right, #33FF57, #33FFB2)",
-              }}
-            ></div>
-          </div>
-          <div className={`range ${rangeHandler(40, 60) ? "active" : ""}`}>
-            <div className="text">{accuracy}</div>
-            <div
-              className="box"
-              style={{
-                background: "linear-gradient(to right, #FFE833, #00FF00)",
-              }}
-            ></div>
-          </div>
-          <div className={`range ${rangeHandler(20, 40) ? "active" : ""}`}>
-            <div className="text">{accuracy}</div>
-            <div
-              className="box"
-              style={{
-                background: "linear-gradient(to right, #FF8333, #FFB233)",
-              }}
-            ></div>
-          </div>
-          <div className={`range ${rangeHandler(0, 20) ? "active" : ""}`}>
-            <div className="text">{accuracy}</div>
-            <div
-              className="box"
-              style={{ background: "linear-gradient(to right, red, #FF5733)" }}
-            ></div>
-          </div>
+          {Object.keys(closestAddress).length > 0 && (
+            <div>
+              <h4>Closest Address:</h4>
+              <div>Name: {closestAddress.Name}</div>
+              <div>Description: {closestAddress.Description}</div>
+              <div>Branch Type: {closestAddress.BranchType}</div>
+              <div>Delivery Status: {closestAddress.DeliveryStatus}</div>
+              <div>Circle: {closestAddress.Circle}</div>
+              <div>District: {closestAddress.District}</div>
+              <div>Division: {closestAddress.Division}</div>
+              <div>Region: {closestAddress.Region}</div>
+              <div>Block: {closestAddress.Block}</div>
+              <div>State: {closestAddress.State}</div>
+              <div>Country: {closestAddress.Country}</div>
+              <div>Pincode: {closestAddress.Pincode}</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
